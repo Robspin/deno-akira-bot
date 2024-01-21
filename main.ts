87c89cd522e4b3c18d780eb5db756f9f56148a60
Robspin <robin_steeman@hotmail.com>
2024-01-21 12:32:15 +0100

    cron attempt

--- main.ts
+++ main.ts
@@ -1,15 +1,23 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts"
+// import { Application, Router } from "https://deno.land/x/oak/mod.ts"
+//
+// const port = 8000
+// const app = new Application()
+// const router = new Router()
+//
+// router.get('/', async (ctx) => {
+//     ctx.response.body = 'deno akira bot running... 🏍️'
+// })
+//
+// app.use(router.routes())
+// app.use(router.allowedMethods())
+//
+// console.log('Running on port: ', port)
+// await app.listen({ port })
 
-const port = 8000
-const app = new Application()
-const router = new Router()
+import { runStrategy } from './strategies/original-cloud-fractal-strategy.ts'
 
-router.get('/', async (ctx) => {
-    ctx.response.body = 'deno akira bot running... 🏍️'
-})
-
-app.use(router.routes())
-app.use(router.allowedMethods())
-
-console.log('Running on port: ', port)
-await app.listen({ port })
+Deno.cron("Run strategy", "00 * * * *", async () => {
+    await runStrategy()
+}, {
+    backoffSchedule: [1000, 5000, 10000],
+});
