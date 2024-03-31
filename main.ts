06b76d514ab32ce14122fb14b7234363a90ca526
Robspin <robin_steeman@hotmail.com>
2024-03-31 13:43:12 +0900

    deploy test

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import { env } from './constants.ts'
 
 
 Deno.cron("Run strategy", env.STRATEGY_CRON_SETTINGS ?? '', async () => {
+
     await runStrategy()
 }, {
     backoffSchedule: [1000, 5000, 10000],
