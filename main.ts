36eb251c2e969ede9e38ed358de18d4642a47074
Robspin <robin_steeman@hotmail.com>
2024-04-06 14:03:01 +0900

    removed async func

--- main.ts
+++ main.ts
@@ -2,8 +2,8 @@ import { runStrategy } from './strategies/original-cloud-fractal-strategy.ts'
 import { env } from './constants.ts'
 
 
-Deno.cron("Run strategy", env.STRATEGY_CRON_SETTINGS ?? '', async () => {
-    await runStrategy()
+Deno.cron("Run strategy", env.STRATEGY_CRON_SETTINGS ?? '', () => {
+    runStrategy()
 }, {
     backoffSchedule: [1000, 5000, 10000]
 })
