2e1fdb6d920ef39f5c1381528cb9b4df994f789f
Robspin <robin_steeman@hotmail.com>
2024-04-06 14:01:56 +0900

    added function

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 import { runStrategy } from './strategies/original-cloud-fractal-strategy.ts'
 import { env } from './constants.ts'
 
-await runStrategy()
 
-// Deno.cron("Run strategy", env.STRATEGY_CRON_SETTINGS ?? '', async () => {
-// }, {
-//     backoffSchedule: [1000, 5000, 10000]
-// })
+Deno.cron("Run strategy", env.STRATEGY_CRON_SETTINGS ?? '', async () => {
+    await runStrategy()
+}, {
+    backoffSchedule: [1000, 5000, 10000]
+})
