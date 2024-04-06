f353c3161e3ed8b5fb2afd87b0afa05dfc4d07ab
Robspin <robin_steeman@hotmail.com>
2024-04-06 14:18:13 +0900

    should be fixed

--- main.ts
+++ main.ts
@@ -2,15 +2,6 @@ import { runStrategy } from './strategies/original-cloud-fractal-strategy.ts'
 import { env } from './constants.ts'
 
 
-// Deno.cron("Run strategy", env.STRATEGY_CRON_SETTINGS ?? '', () => {
-//     runStrategy()
-// }, {
-//     backoffSchedule: [1000, 5000, 10000]
-// })
-
-// console.log(env.STRATEGY_CRON_SETTINGS)
-
 Deno.cron("Run strategy", env.STRATEGY_CRON_SETTINGS ?? "04 * * *", async () => {
     await runStrategy()
-})
-// }, { backoffSchedule: [1000, 5000, 10000] });
+}, { backoffSchedule: [1000, 5000, 10000] })
