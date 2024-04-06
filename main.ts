2203edc73f267ff7f1b506353c724e36050845fb
Robspin <robin_steeman@hotmail.com>
2024-04-06 14:12:01 +0900

    test the cron

--- main.ts
+++ main.ts
@@ -2,8 +2,12 @@ import { runStrategy } from './strategies/original-cloud-fractal-strategy.ts'
 import { env } from './constants.ts'
 
 
-Deno.cron("Run strategy", env.STRATEGY_CRON_SETTINGS ?? '', () => {
-    runStrategy()
-}, {
-    backoffSchedule: [1000, 5000, 10000]
-})
+// Deno.cron("Run strategy", env.STRATEGY_CRON_SETTINGS ?? '', () => {
+//     runStrategy()
+// }, {
+//     backoffSchedule: [1000, 5000, 10000]
+// })
+
+Deno.cron("Run once a minute", "* * * * *", () => {
+    console.log("Hello, cron!");
+});
