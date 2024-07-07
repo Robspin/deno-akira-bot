d61c45467f416b370ec2cb51799985726aa6fd19
Robspin <robin_steeman@hotmail.com>
2024-07-07 18:09:58 +0800

    updated notification logic

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
 import { env } from './constants.ts'
 import runStrategy from './strategies/ichimoku-williams-long.ts'
 
-await runStrategy()
-// Deno.cron("Run strategy", env.STRATEGY_CRON_SETTINGS ?? '', async () => {
-//     await runStrategy()
-// })
+Deno.cron("Run strategy", env.STRATEGY_CRON_SETTINGS ?? '', async () => {
+    await runStrategy()
+})
